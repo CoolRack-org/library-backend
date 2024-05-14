faab75592b47482bbc26c721b2b2ffce245eb72a
Josep <je.sendra@ever-si.es>
2024-05-14 17:04:12 +0200

    Removed models temporarily

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,25 +1,6 @@
 import { Request, Response } from "express";
 
-import { book } from "../models/book";
-
 const express = require("express");
 const router = express.Router();
-
-// Define routes
-
-router.get("/", (req: Request, res: Response) => {
-  const newBook: book = {
-    id: "1",
-    author: "",
-    torrentHash: "",
-    publicationDate: new Date(),
-    pages: 0,
-    title: "",
-    description: "",
-    sinopsis: "",
-  };
-
-  res.send(newBook);
-});
 
 module.exports = router;
