b30fad842bc14b07421c7e737004279fc5a254c9
Josep <je.sendra@ever-si.es>
2024-05-14 17:47:44 +0200

    Book controller v1

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -1,6 +1,33 @@
-import { Request, Response } from "express";
-
-const express = require("express");
+// Import express
+import express, { Request, Response } from "express";
 const router = express.Router();
 
+// Import Prisma Client
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+// Get all books
+router.get("/", async (req: Request, res: Response) => {
+  const books = await prisma.book.findMany();
+  res.json(books);
+});
+
+// Get a book by id
+router.get("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const book = await prisma.book.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+  res.json(book);
+});
+
+// Create a book
+router.post("/", async (req: Request, res: Response) => {
+  const { book } = req.body;
+  res.json(book);
+});
+
+// Export the router
 module.exports = router;
